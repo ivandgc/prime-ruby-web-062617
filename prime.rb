6280deef024a9070d7a47912469a769dfb0ba5e2
Ivan Garcia <ivandgc@hotmail.com>
2017-06-24 02:06:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime? (int)
+  state =
+  array = (2...int).to_a
+  array.each do |var|
+    int % var == 0 ? state = false : state = true
+    break if state == false
+  end
+  if int < 2
+    state = false
+  end
+  state
+end
